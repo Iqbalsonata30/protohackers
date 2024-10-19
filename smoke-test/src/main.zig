a98d9f416b8328e001d50ebefb45d186a1b948a2
iqbalsonata <iqbalsonata2@gmail.com>
2024-10-19 14:50:47 +0700

    sent message back to client, and accept multiple connections

--- smoke-test/src/main.zig
+++ smoke-test/src/main.zig
@@ -20,18 +20,28 @@ pub fn main() !void {
     try posix.bind(socket_fd, @ptrCast(&server_addr), @sizeOf(@TypeOf(server_addr)));
     try posix.listen(socket_fd, @as(u31, 5));
 
-    const client_fd = try posix.accept(socket_fd, @ptrCast(&client_addr), &client_addr_len, 0);
-    defer posix.close(client_fd);
-    if (client_fd == -1) {
-        std.debug.panic("failed to accept connection", .{});
-    }
+    while (true) {
+        const client_fd = try posix.accept(socket_fd, @ptrCast(&client_addr), &client_addr_len, 0);
+        defer posix.close(client_fd);
+        if (client_fd == -1) {
+            std.debug.panic("failed to accept connection", .{});
+        }
 
-    const data_received = try posix.recv(client_fd, &buffer, 0);
-    if (data_received < 0) {
-        std.debug.panic("data has no received", .{});
+        const data_received = try posix.recv(client_fd, &buffer, 0);
+        if (data_received < 0) {
+            std.debug.panic("data has no received", .{});
+        }
+        const message = buffer[0..data_received];
+        std.debug.print("from client : {any}\n", .{client_addr});
+        std.debug.print("received message: {s}", .{message});
+
+        const data_sent = try posix.send(client_fd, message, 0);
+        if (data_sent < 0) {
+            std.debug.panic("failed to send message", .{});
+        }
+        std.debug.print("sent to client : {any}\n", .{client_addr});
+        std.debug.print("sent message : {s}\n", .{message});
     }
-    const message = buffer[0..data_received];
-    std.debug.print("{s}\n", .{message});
 }
 
 fn handleConnection() void {}
