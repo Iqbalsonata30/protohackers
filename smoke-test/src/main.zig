f789fa58201087daff226968622cd8d9f70b15c7
iqbalsonata <iqbalsonata2@gmail.com>
2024-10-22 00:33:51 +0700

    solved the smoke test problem

--- smoke-test/src/main.zig
+++ smoke-test/src/main.zig
@@ -1,3 +1,9 @@
+// 1. Accept TCP Connections -> passed
+// 2. Handle at least 5 connections -> passed
+// 3. Receive message and send it back unmodified. ->passed
+//
+// What is TCP Echo Service : https://www.rfc-editor.org/rfc/rfc862.htl
+
 const std = @import("std");
 const posix = std.posix;
 const assert = std.debug.assert;
@@ -17,45 +23,44 @@ pub fn main() !void {
     if (@hasDecl(posix.SO, "REUSEPORT")) {
         try posix.setsockopt(srv_fd, posix.SOL.SOCKET, posix.SO.REUSEPORT, &std.mem.toBytes(@as(c_int, 1)));
     }
+
     try posix.bind(srv_fd, &server_addr.any, @sizeOf(@TypeOf(server_addr)));
     try posix.listen(srv_fd, 10);
-    std.debug.print("server addr: {any}\n", .{server_addr});
 
     var client_addr = net.Address.initIp4(.{ 0, 0, 0, 0 }, 0);
     var client_addr_len: posix.socklen_t = @sizeOf(@TypeOf(client_addr));
-    client_fd = try posix.accept(srv_fd, @ptrCast(&client_addr), &client_addr_len, 0);
-    defer posix.close(client_fd);
-    while (true) {
-        if (client_fd == -1) {
-            std.debug.panic("failed to accept connection", .{});
-        }
+
+    std.debug.print("server running on port {d}\n", .{server_addr.getPort()});
+    client_fd = try posix.accept(srv_fd, &client_addr.any, &client_addr_len, 0);
+    while (client_fd > 0) : (client_fd = try posix.accept(srv_fd, &client_addr.any, &client_addr_len, 0)) {
         const thread = try std.Thread.spawn(.{}, worker, .{client_fd});
         defer thread.join();
     }
 }
 
 fn worker(client_fd: posix.socket_t) !void {
-    var m = std.Thread.Mutex{};
+    std.debug.print("worker starting...\n", .{});
+
     var data_size: usize = 0;
     var buffer: [1024]u8 = undefined;
+
     data_size = try posix.recv(client_fd, &buffer, 0);
-    while (data_size > 0) : (data_size = try posix.recv(client_fd, &buffer, 0)) {
-        m.lock();
-        defer m.unlock();
-        const data_sent = try posix.send(client_fd, &buffer, 0);
-        std.debug.print("sent message : {any}\n", .{data_sent});
+    while (data_size > 0) {
+        std.debug.print("size of the data : {d}\n", .{data_size});
+
+        const message = buffer[0..data_size];
+        const data_sent = try posix.send(client_fd, message, 0);
+        std.debug.assert(data_sent != 0);
+
+        std.debug.print("received message : {s}", .{message});
+        std.debug.print("sent message : {s}", .{message});
+        data_size = try posix.recv(client_fd, &buffer, 0);
+    }
+
+    if (data_size < 0) {
+        std.debug.panic("received failed\n", .{});
+    } else {
+        std.debug.print("client disconnect\n", .{});
     }
     defer posix.close(client_fd);
 }
-
-// send data to a server and echo the data back
-// receive data from a client, send it back unmodified
-// 1. Accept TCP Connections
-// 2. Handle at least 5 connections
-//
-// Implement TCP Echo Service : https://www.rfc-editor.org/rfc/rfc862.htl
-//
-//
-// next find way to handle 5 simultaneos connections
-// why i need thread, if i met the end of bytes, i kill the connection,
-//
