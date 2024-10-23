57af0577adeccefd63fc42315558a2e42f66648e
iqbalsonata <iqbalsonata2@gmail.com>
2024-10-23 20:00:16 +0700

    create tcp connection

--- prime-time/src/main.zig
+++ prime-time/src/main.zig
@@ -1,24 +1,59 @@
+// Prime Time
+//
+//  Accept TCP Connection
+//  Whenever receive a conforming request, send back a correct response and wait for another request
+//  Whenever you receive a malformed request, send back a single malformed response, and disconnect the client.
+//  Handle at least 5 Connection
+//
+//  Example Response
+//  {"method":"isPrime","prime":false}
+//
+
 const std = @import("std");
+const posix = std.posix;
+const mem = std.mem;
+
+const IPv4 = extern struct {
+    sa: posix.sockaddr.in,
+
+    pub fn init(addr: [4]u8, port: u16) IPv4 {
+        return IPv4{ .sa = .{
+            .port = mem.nativeToBig(u16, port),
+            .addr = @as(*align(1) const u32, @ptrCast(&addr)).*,
+        } };
+    }
+
+    pub fn format(self: IPv4, comptime fmt: []const u8, options: std.fmt.FormatOptions, out_stream: anytype) !void {
+        if (fmt.len != 0) std.fmt.invalidFmtError(fmt, self);
+        _ = options;
+        const bytes = @as(*const [4]u8, @ptrCast(&self.sa.addr));
+        try std.fmt.format(out_stream, "{}.{}.{}.{}:{}", .{
+            bytes[0],
+            bytes[1],
+            bytes[2],
+            bytes[3],
+            mem.bigToNative(u16, self.sa.port),
+        });
+    }
+};
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const sock_addr = IPv4.init([4]u8{ 0, 0, 0, 0 }, 1234);
+    const sock_addr_len = @sizeOf(posix.sockaddr.in);
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const sock_fd = try posix.socket(posix.AF.INET, posix.SOCK.STREAM, 0);
+    defer posix.close(sock_fd);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    _ = try posix.bind(sock_fd, @ptrCast(&sock_addr), sock_addr_len);
+    _ = try posix.listen(sock_fd, 5);
 
-    try bw.flush(); // don't forget to flush!
-}
+    var peer_socket = IPv4.init([4]u8{ 0, 0, 0, 0 }, 1234);
+    var peer_socket_len: u32 = @sizeOf(@TypeOf(peer_socket));
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    var conn: posix.socket_t = try posix.accept(sock_fd, @ptrCast(&peer_socket), &peer_socket_len, 0);
+    defer posix.close(conn);
+
+    while (conn > 0) : (conn = try posix.accept(sock_fd, @ptrCast(&peer_socket), &peer_socket_len, 0)) {
+        std.debug.print("address:{any} is connecting\n", .{peer_socket});
+    }
 }
